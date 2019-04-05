9fce1106a695e937a9b78dcaf1ecca67ed88a99b
Nick Fairneny <nickfairneny82@gmail.com>
2019-04-05 00:29:00 +0000

    I'm returning this crack fork

--- Makefile
+++ Makefile
@@ -3,17 +3,17 @@
 # This rule links hashpass.o and md5.o along with the
 # libssl and libcrypto libraries to make the executable.
 hashpass: hashpass.o md5.o
-	clang hashpass.o md5.o -o hashpass -l crypto
+	clang hashpass.o md5.o -o hashpass -l crypto -g -Wall
 
 # Add recipe to compile md5.o from md5.c below,
 # indented with a tab.
 md5.o: md5.c
-
+	clang -c md5.c
 
 # Add recipe to compile hashpass.o from hashpass.c below,
 # indented with a tab.
 hashpass.o: hashpass.c
-
+	clang -c hashpass.c
 
 hashes: hashpass
 	./hashpass rockyou100.txt hashes.txt
@@ -26,3 +26,4 @@ check: hashpass
 
 clean:
 	rm -f *.o hashpass hashes.txt
+	
---
+++ hashpass.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "md5.h"
+
+int main(int argc, char *argv[])
+{
+	FILE *in = fopen(argv[1], "r");
+    FILE *out = fopen(argv[2], "w"); 
+    
+    if(!in)
+    {
+        printf("Can't open %s for reading\n", argv[1]);
+        exit(1);
+    }
+    
+    if (!out)
+    {
+        printf("Can't open %s for writing\n", argv[2]);
+        exit(1);
+    }
+    
+    char password[20]; 
+
+	while (fgets(password, 20, in) != NULL)
+	{
+	    char *hash = md5(password, strlen(password) -1);
+	    //(hash, 38, in);
+    	//printf("Hash: %s\n", hash);
+    	fprintf(out, "%s\n", hash);
+    	free(hash);
+    	
+    }
+    
+    fclose(in);
+    fclose(out);
+    
+}    
